f581cf5c2231cfd4025ce5c7f35332207300653f
Ankur Aggarwal <ankur.aggarwal2390@gmail.com>
2011-12-30 18:53:00 +0530

    Adding Sound Demo

---
+++ soundPlay.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+############################################################################
+# File name :soundPlay.py
+# Purpose : Demostrating Audio File Involvement In A Pygame Program 
+# Start date : 30/12/2011
+# End date : 30/12/2011
+# Author : Ankur Aggarwal
+# License : GNU GPL v3 http://www.gnu.org/licenses/gpl.html
+# Reference : Game Programming By Andy Harris
+# Dependancy : yikes.ogg 
+############################################################################
+
+import pygame
+from pygame.locals import *
+
+
+pygame.init()
+pygame.mixer.init() #mixer module Intialization
+
+def main():
+  screen=pygame.display.set_mode((640,480),0,24)
+  pygame.display.set_caption('Sound Testing')
+  sound=pygame.mixer.Sound('yikes.ogg') #creating an audio object
+  font=pygame.font.SysFont('arial',30)
+  soundFont=font.render("Press Space To Hear A Sound",True,(0,255,0)) 
+  while 1:
+      pressed=pygame.key.get_pressed()
+      for i in pygame.event.get():
+          if i.type==QUIT or pressed[K_q]:
+              exit()
+          elif pressed[K_SPACE]:
+              sound.play()  #time to play
+      screen.blit(soundFont,(100,100))
+      pygame.display.flip()
+
+
+if __name__=='__main__':
+    main()
